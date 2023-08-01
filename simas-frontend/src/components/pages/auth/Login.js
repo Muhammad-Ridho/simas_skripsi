69b645958472790517970f2c362248ebb462898a
Muhammad-Ridho <ramadhanjenius1@gmail.com>
2023-08-02 00:44:39 +0700

    simas frontend

---
+++ simas-frontend/src/components/pages/auth/Login.js
@@ -0,0 +1,9 @@
+import LoginPage from "./LoginPage";
+
+function Login() {
+    return(
+        <LoginPage />
+    )
+}
+
+export default Login;
---
+++ simas-frontend/src/components/pages/auth/LoginPage.jsx
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+class LoginPage extends Component {
+    render() {
+        return (
+            <div>
+                <h2>PAGE FORM LOGIN</h2>
+            </div >
+        )
+    }
+}
+
+export default LoginPage;
